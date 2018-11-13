2553b88c3d890e505294adb76d721f94d1b843b9
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-11-13 14:26:00 -0800

    add timestamps to new tasks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
+import timestamp from 'time-stamp';
 import { SAVE_TODO, REMOVE_TASK } from './types';
 
+
 export function saveTodo(todo) { 
-  return { type: SAVE_TODO, payload: todo };
+  const payload = {...todo, created_at: timestamp.utc('YYYYMMDDHHmmssms')};
+  return { type: SAVE_TODO, payload };
 }
 
 export function removeTask(id) {
