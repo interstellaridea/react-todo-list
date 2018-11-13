53c5eb1752805fdb38c789cbff61472468f3b518
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-11-12 19:24:07 -0800

    Redux boilerplate

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools()
+  );
+
 ReactDOM.render(
-  <Provider store={ createStore(reducers, {}) }>
+  <Provider store={ store }>
     <App />
   </Provider>,
   document.querySelector("#root")
