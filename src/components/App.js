84f2f35224f9e2a51764a19780c41577e41d25c8
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-12-15 21:24:18 -0800

    style for formik input with material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import emoji from 'react-easy-emoji'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import TodoProvider from '../TodoProvider';
 import ShowTodos from './ShowTodos';
 import NewTodo from './NewTodo';
 
+const styles = {
+  root: {
+    flexGrow: 1,
+    
+  },
+  newTodo: {
+    textAlign: 'center',
+    
+  }
+}
+
+/// onClick this return
+// if (true)
+
+
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return(
-      <div>
+      <Fragment>
+        <Grid container justify='center' className={classes.root}>
+        <Typography variant='headline'>
+        { emoji('Cached Tasks 🍪')}
+        </Typography>
+        <CssBaseline/>
         <TodoProvider>
-          <NewTodo />
+          <Grid item className={classes.newTodo} sm={12}>
+            <NewTodo />
+          </Grid>
           <ShowTodos />
         </TodoProvider>
-      </div>
+        </Grid>
+      </Fragment>
     );
   }
 };
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,31 +1,46 @@
 import React from 'react'
 import { Formik, Form, Field } from 'formik';
+import { TextField, Button, withStyles } from '@material-ui/core';
 import { TodoContext } from '../TodoContext';
 
-export default () => 
+const styles = theme => ({
+  form: {
+    color: 'red',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    display: 'flex',
+    marginBottom: '25px'
+  }
+})
+export default withStyles(styles)(({classes}) => 
   <TodoContext.Consumer>
     { ({addTodo}) => (
-      <TodoContext.Consumer>
-      { context => (
-        <Formik
-          initialValues={{ todo: "" }}
-          onSubmit={(values, actions) => {
-            
-            addTodo(values);
-            actions.setSubmitting(false);
-            actions.resetForm();
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field name="todo" type="text" placeholder="Name of Todo" />
-              <button disabled={isSubmitting} type="submit">
-                Add todo
-              </button>
-            </Form>
-          )}
-        </Formik>
-      )}
-    </TodoContext.Consumer>
+      <Formik
+        initialValues={{ todo: "" }}
+        onSubmit={(values, actions) => {
+          addTodo(values);
+          actions.setSubmitting(false);
+          actions.resetForm();
+        }}
+      >
+        {({ values, isSubmitting, handleChange }) => (
+          <Form className={classes.form} >
+            <Field name='todo' type='text' render={props => (
+              <TextField
+                {...props}
+                value={values.todo}
+                onChange={handleChange('todo', values)}
+                label="Todo"
+                margin="normal"
+              />
+            )}/>
+            <Button type="submit" disabled={isSubmitting}>
+              Add todo
+            </Button>
+          </Form>
+        )}
+      </Formik>
     )}
   </TodoContext.Consumer>
+)
