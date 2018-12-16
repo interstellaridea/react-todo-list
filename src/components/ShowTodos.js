ff12f38a84757264ead64c92ff31f48f12c77d60
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-12-15 22:02:32 -0800

    Convert to stateless fn component and style

--- src/components/ShowTodos.js
+++ src/components/ShowTodos.js
@@ -1,33 +1,41 @@
-import React, { Component, Fragment } from "react";
+import _ from 'lodash';
+import React from "react";
 import { RIEInput } from 'riek2';
+import  IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import { TodoContext } from "../TodoContext";
 
-class ShowTodos extends Component {
-
-  handleUpdateChange = ({todo: updated_value }, key, updateTodo) => {
-    updateTodo(key,updated_value);
-  }
-
-  render() {
-    return (
-      <TodoContext.Consumer>
-        { ({todo_list, updateTodo, removeTodo}) => (
-          <Fragment>
-            {Object.keys(todo_list).map(key => (
-              <div key={key}>
-                <RIEInput
-                  change={ e => this.handleUpdateChange(e, key, updateTodo)}
-                  value={todo_list[key].todo}
-                  propName='todo'
-                />
-                <button onClick={ () => removeTodo(key)}>&times;</button>
-              </div>
-              ))}
-          </Fragment>
-        )}
-      </TodoContext.Consumer>
-    )
-  }
+const handleUpdateChange = ({todo: updated_value }, key, updateTodo) => {
+  updateTodo(key,updated_value);
 }
 
-export default ShowTodos;
+export default () => {
+  return (
+    <TodoContext.Consumer>
+    { ({todo_list, updateTodo, removeTodo}) => (
+      <List component='nav'>
+        {Object.keys(todo_list).map(key => (
+          <ListItem key={key}>
+            <ListItemText>
+              <RIEInput
+                change={ e => handleUpdateChange(e, key, updateTodo)}
+                value={_.capitalize( todo_list[key].todo )}
+                propName='todo'
+                />
+            </ListItemText>
+              <ListItemIcon>
+                <IconButton onClick={ () => removeTodo(key) } aria-label="Delete">
+                  <DeleteIcon fontSize="small" />
+                </IconButton>                
+            </ListItemIcon>
+          </ListItem>
+          ))}
+      </List>
+    )}
+  </TodoContext.Consumer>    
+  )
+}
